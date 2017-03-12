d7110d5c012356236f88f0465a0a032e3f167686
shishao-in139 <shishaoqi0928@gmail.com>
2017-03-13 01:52:20 +0800

    start python web

---
+++ app.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# @Author: shishao
+# @Date:   2017-03-12 10:50:39
+# @Last Modified by:   shishao
+# @Last Modified time: 2017-03-12 10:59:37
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return '<h1>Home</h1>'
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return '''<form action="/signin" method="post">
+              <p><input name="username"></p>
+              <p><input name="password" type="password"></p>
+              <p><button type="submit">Sign In</button></p>
+              </form>'''
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    # 需要从request对象读取表单内容：
+    if request.form['username']=='admin' and request.form['password']=='password':
+        return '<h3>Hello, admin!</h3>'
+    return '<h3>Bad username or password.</h3>'
+
+if __name__ == '__main__':
+    #app.run()
+    app.run(host='192.168.1.139',port=5000)
